087caef5ff4724fd42bfcb195b7814e71a9a3238
Eldarian <eldarian3@gmail.com>
2020-12-29 19:06:19 +0300

    Enemy Death with FX

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject enemyDeathFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        BoxCollider collider = gameObject.AddComponent<BoxCollider>();
+        collider.isTrigger = false;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Instantiate(enemyDeathFX, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
