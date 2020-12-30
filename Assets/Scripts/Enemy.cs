74aed1df2dfa3230ea972455f62a84b980480a63
Eldarian <eldarian3@gmail.com>
2020-12-30 17:23:19 +0300

    Score UI

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject enemyDeathFX;
+    [SerializeField] int scorePerHit;
+
+    ScoreBoard scoreBoard;
 
     // Start is called before the first frame update
     void Start()
     {
         AddNonTriggerBoxCollider();
+        scoreBoard = GameObject.Find("Score Text").GetComponent<ScoreBoard>();
     }
 
     private void AddNonTriggerBoxCollider()
@@ -20,7 +24,10 @@ public class Enemy : MonoBehaviour
 
     private void OnParticleCollision(GameObject other)
     {
-        Instantiate(enemyDeathFX, transform.position, Quaternion.identity);
+        scoreBoard.ScoreHit(scorePerHit);
+        Instantiate(enemyDeathFX, transform.position, Quaternion.identity, GameObject.Find("Spawned Objects").transform);
         Destroy(gameObject);
     }
+
+    
 }
---
+++ Assets/Scripts/SelfDestructor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructor : MonoBehaviour
+{
+    [Tooltip("In seconds")] [SerializeField] float SelfDestructionTime = 5f;
+
+    void Start()
+    {
+        Destroy(gameObject, 5f);
+    }
+
+}
